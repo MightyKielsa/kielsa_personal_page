e5f3fa8a26b5009cc9cd50da0d359ea3069cc5e4
MightyKielsa <104023970+MightyKielsa@users.noreply.github.com>
2022-07-12 16:59:40 +0100

    cards more or less done

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -2,6 +2,7 @@
 	import Card, { Actions, Content } from '@smui/card';
 	import Button, { Label } from '@smui/button';
 	//import IconButton, { Icon } from '@smui/icon-button';
+	export let projectObj;
 
 	function onClick() {
 		return;
@@ -10,17 +11,53 @@
 
 <main>
 	<div class="card-container">
-		<Card>
-			<Content>A card with a full-bleed action.</Content>
+		<Card class="card-class">
+			<img src={projectObj.imgSrc} alt={projectObj.imgAlt} />
+			<div class="text-container">
+				<h3>{projectObj.projectTitle}</h3>
+				<Content>{projectObj.projectDescription}</Content>
+			</div>
 			<Actions fullBleed>
-				<Button on:click={() => clicked++}>
-					<Label>Action</Label>
-					<i class="material-icons" aria-hidden="true">arrow_forward</i>
-				</Button>
+				<div class="button-div">
+					<Button on:click={onClick} variant="unelevated">
+						<Label>View Project</Label>
+						<i class="material-icons" aria-hidden="true">arrow_forward</i>
+					</Button>
+				</div>
 			</Actions>
 		</Card>
 	</div>
 </main>
 
 <style>
+	.card-container {
+		max-width: 300px;
+		box-shadow: 5px 5px 10px 2px rgba(0, 0, 0, 0.8);
+		border-radius: 3%;
+		height: auto;
+	}
+	.card-class {
+		background-color: #ffc107;
+	}
+	.text-container {
+		display: flex;
+		flex-direction: column;
+		height: 15vh;
+	}
+
+	h3 {
+		text-align: center;
+	}
+	img {
+		border-radius: 3%;
+		width: 10vw;
+		height: auto;
+		margin-left: auto;
+		margin-right: auto;
+		margin-top: 10%;
+	}
+	.button-div {
+		width: 50%;
+		margin: 2vh auto 2vh auto;
+	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,48 @@
 <script>
 	import SkillContainer from '../lib/SkillContainer.svelte';
 	import ProjectCard from '../lib/ProjectCard.svelte';
+
+	let projectsArr = [
+		{
+			imgSrc: '/project1-pic.jpg',
+			imgAlt: 'A laptop, smartphone and notebook lying on a desk',
+			projectTitle: 'ONE WEEK PROJECT',
+			projectDescription:
+				"A 'Bootcamper Helper app made during my time at School of code, the timeframe for the project was 5 days"
+		},
+		{
+			imgSrc: '/project2-pic.jpg',
+			imgAlt: 'A screen displaying computer code',
+			projectTitle: 'FOUR WEEK PROJECT',
+			projectDescription:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		},
+		{
+			imgSrc: '/project3-pic.jpg',
+			imgAlt: 'A personal computer screen displaying a futuristic app',
+			projectTitle: 'TOTALLY LEGIT PROJECT',
+			projectDescription:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+		}
+	];
 </script>
 
 <main>
 	<SkillContainer />
-	<ProjectCard />
+	<div class="projects-row">
+		{#each projectsArr as project}
+			<ProjectCard projectObj={project} />
+		{/each}
+	</div>
 </main>
 
 <style>
+	main {
+		height: 300vh;
+	}
+	.projects-row {
+		display: flex;
+		justify-content: space-around;
+		height: 50vh;
+	}
 </style>
